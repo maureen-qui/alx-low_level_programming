9b246bcd319bbf5ccc703ef4ee06b8c6815db6f8
maureen-qui <maureenwangui08@yahoo.com>
2022-10-18 12:35:08 -0700

    only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,8 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * print_last_digit - a function that prints the last digit of a number
- *  @nld: number's last digit result
- *   Return: value of the last digit
+ * @nld: number's last digit result
+ * Return: value of the last digit
  */
 int print_last_digit(int nld)
 {
