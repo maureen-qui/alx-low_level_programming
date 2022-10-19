ccadd328dd7f87f3eb0cd3d25aa067b4966af7d8
maureen-qui <maureenwangui08@yahoo.com>
2022-10-18 21:35:02 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
-#include < stdio.h >
-#include < stdlib.h >
-#include < time.h >
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main -prints if the number is positive,negative or 0
- * Return: Always 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,17 +12,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
-	{
-		printf("%d is zero\n", n);
-	}
+		printf("%d is positive\n", n);
 	else if (n == 0)
-	{
 		printf("%d is zero\n", n);
-	}
 	else
-	{
 		printf("%d is negative\n", n);
-	}
-	return (0)
+
+
+
+	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,29 @@
 #include < stdio.h >
 #include < stdlib.h >
-#include < time.h >
-
+#include < time.h >;
 /**
- * main - Entry
- * Return: Always 0
+ * main - entry point
+ * Description: prints if a number is positive or negative
+ * Return: 0
  */
 int main(void)
 {
 	int n;
-	int a;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	a = n % 10;
-
-	if (n > 5)
+	if (n > 0)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, a);
+		printf ("%d is positive\n", n);
 	}
-	else if (a == 0)
+	else if (n == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, a);
-	}
-	else
-	{printf("Last digit of % is %d and is less than 6 and not 0\n, n, a);
-	}
-	return (0);
+		printf ("%d is zero\n", n);
+  	}
+  	else if (n < 0)
+	{
+	        printf ("%d is negative\n", n);
+	} 
+        return(0);
 }
+
