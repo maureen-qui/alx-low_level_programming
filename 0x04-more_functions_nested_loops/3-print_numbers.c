0caffe28b2bbdcef27e680ce4b411a3d10e25c43
maureen-qui <maureenwangui08@yahoo.com>
2022-10-23 23:17:47 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,17 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_numbers - a function that prints the numbers, from 0 to 9
- * 
- * Return: 0-9 followed by newline
+ * print_numbers -  checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
  */
 void print_numbers(void)
 {
-	int i;
+	int a;
 
-	for (i = 0; i <= 9; i++)
-		_putchar(i + '0');
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
 	_putchar('\n');
 }
