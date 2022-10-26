8105dfc7ce4f7182f4d9c77262f1eb61d9934157
maureen-qui <maureenwangui08@yahoo.com>
2022-10-26 08:37:03 -0700

    https://github.com/maureen-qui/alx-low_level_programming.git

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,25 +1,24 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_rev - Prints a string in reverse
- * @s: The string to print
- *
- * Return: void
+ *  print_rev - a function that prints a string, in reverse
+ *  fcounter is to first count to end, n is to count back
+ *   @s: str input
+ *   Return: string in reverse
  */
 void print_rev(char *s)
 {
-	int c = 0;
+	int fcounter = 0;
+	int i, n;
 
-	while (s[c] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		c++;
+		fcounter++;
 	}
 
-	for (c -= 1; c >= 0; c--)
+	for (n = (fcounter - 1); n >= 0; n--)
 	{
-		_putchar(s[c]);
+		_putchar(s[n]);
 	}
-
 	_putchar('\n');
 }
