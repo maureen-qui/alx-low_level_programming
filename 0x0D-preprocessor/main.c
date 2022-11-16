e692b19e2db9983e1157c222742009ffe6825a28
maureen-qui <maureenwangui08@yahoo.com>
2022-11-16 05:44:33 -0800

    0x0d preprocesor

---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,11 @@
+#ifndet MAIN_C
+#define MAIN_C
+
+#include <stdio.h>
+#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "2-main.c"
+#include "3-function_like_macro.h"
+#include "4-sum.h"
+
+#endif
