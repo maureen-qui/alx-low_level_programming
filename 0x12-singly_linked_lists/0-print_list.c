ea026625dafc3766557af9e7b17b0b5d67f6ec1c
maureen-qui <maureenwangui@ymail.com>
2022-11-29 01:00:14 -0800

    update 2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,33 +1,23 @@
-/*
- * File: 0-print_list.c
- * Auth: Maureen W Wamwea
- */
-
-#Include "lists.h"
-#include <stdio.h>
-
+#include "lists.h"
 /**
  * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
- *
- * Return: The number of nodes in h.
+ * @h: Singly linked list.
+ * Return: Number of elements in the list.
  */
 
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
-size_t nodes = 0;
+	size_t nelem;
 
-while (h)
-{
-if (h->str == NULL)
-printf("[0] (nil)\n");
-
-else
-printf("[%d] %s\n", h->len, h->str);
-
-nodes++;
-h = h->next;
-}
-
-return (nodes);
+	nelem = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nelem++;
+	}
+	return (nelem);
 }
