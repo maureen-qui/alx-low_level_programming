0b87c3f9693db9339795163ea5cd4432f339b9d6
maureen-qui <maureenwangui@ymail.com>
2022-11-29 17:40:27 -0800

    updated 2. Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,44 +3,48 @@
 #include <string.h>
 
 /**
- * add_node - Adds a new node at the beginning of a list
+ * add_note - Adds a new node at the beginning of a list
  * @head: The original linked list
  * @str: The string to add to the node
- *
- * Return: The address of the new list or NULL if it failed
+ * Return: The address of the new list or NULL if if failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new;
+list_t *temp;
 
-new = malloc(sizeof(list_t));
-if (new == NULL)
+if (head != NULL && str != NULL)
 {
+temp = malloc(sizeof(list_t));
+if (temp == NULL)
 return (NULL);
+
+temp->str = strdup(str);
+temp->len = _strlen(str);
+temp->next = *head;
+
+*head = temp;
+
+return (temp);
 }
 
-new->str = strdup(str);
-if (!new->str)
-{
-free(new);
-return (NULL);
-}
-new->len = _strlen_recursion(new->str);
-new->next = *head;
-*head = new;
-
-return (new);
+return (0);
 }
 
 /**
- * _strlen_recursion - returns the length of a string.
- * @s: string.
- * Return: length of @s.
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
  */
-int _strlen_recursion(char *s)
+int _strlen(const char *s)
 {
-if (*s == 0)
-return (0);
-else
-return (1 + _strlen_recursion(s + 1));
+int c = 0;
+
+while (*s)
+{
+s++;
+c++;
+}
+
+return (c);
 }
