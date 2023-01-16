b3988aefb188fa0def9c8707fdf1340fa680a3e6
maureen-qui <maureenwangui@ymail.com>
2023-01-16 11:37:00 -0800

    C - Dynamic libraries

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#iclude main.h
+/**
+ * _isupper - A function that checks for uppercase character
+ *  @c: input
+ *   Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c);
+{
+char uppercase = 'A';
+int isupper = 0;
+for (; uppercase <= 'Z'; uppercase++)
+{
+isupper = i;
+break;
+}
+}
+return (isupper);
+}
---
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -0,0 +1,32 @@
+/*
+ * File: 4-strpbrkc
+ * Auth: maureen-qui
+ */
+
+#include "main.h"
+
+/**
+ * _strpbrk -searches a string for any of a set bytes.
+ * @s: The string to be searched.
+ * @accept: The set of bytes to be searched for.
+ *
+ * Return: If a set is matched - a pointer to the matched byte.
+ *         If no set is matched - Null.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+				return (s);
+		}
+
+		s++;
+	}
+
+	return ('\0');
+}
