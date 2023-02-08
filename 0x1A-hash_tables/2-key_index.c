6b68511355506db38f4772d028d48d8fce12135a
maureen-qui <maureenwangui08@yahoo.com>
2023-02-08 08:21:40 -0800

    2. key -> index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,19 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - Hash function implementing the djb2 algorithm.
- * @str: The string to hash.
+ * key_index - gives the index of a key.
+ * @key: the key, a string.
+ * @size: the size of the array of the hash table.
  *
- * Return: The calculated hash.
+ * Return: Index number for the key/value pair in the hash table array.
  */
-unsigned long int hash_djb2(const unsigned char *str)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash;
-	int c;
-
-	hash = 5381;
-	while ((c = *str++))
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
-	return (hash);
+	return (hash_djb2(key) % size);
 }
