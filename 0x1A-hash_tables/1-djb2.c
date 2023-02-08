abe3baec83dad39f9aa039593a08385871ecf02d
maureen-qui <maureenwangui08@yahoo.com>
2023-02-08 07:28:52 -0800

    1. djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,20 +1,20 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - implementation of the djb2 algorithm
+ * hash_djb2 - Hash function, takes a string and turns it into a hash number
+ * @str: the string, a constant value unrepeated in the hash table
  *
- * @prmString: string used to generate hash value
- *
- * Return: hash value
+ * Return: The hash number
  */
-unsigned long int hash_djb2(const unsigned char *prmString)
+unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
 
 	hash = 5381;
 	while ((c = *prmString++))
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
+	{
+	hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+        }
 	return (hash);
 }
